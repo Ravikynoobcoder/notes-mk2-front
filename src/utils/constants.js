46524826203ccad89525049940fab1dbaeabf7f0
Ravi <raviy3083@gmail.com>
2026-03-26 17:13:56 +0530

    backend url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:8000' //backend url
-//export const BASE_URL = 'https://back-azure-beta.vercel.app' //backend url
+//export const BASE_URL = 'http://localhost:8000' //backend url
+export const BASE_URL = 'https://notes-mk2-back.onrender.com' //backend url
